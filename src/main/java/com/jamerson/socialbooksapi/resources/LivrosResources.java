e1f126e4008db442b313662a0012770a030e1d4f
Jamerson Silva <jamerson.silva@acad.ifma.edu.br>
2018-08-03 15:18:56 -0300

    Indentação da classe livrosResources

--- src/main/java/com/jamerson/socialbooksapi/resources/LivrosResources.java
+++ src/main/java/com/jamerson/socialbooksapi/resources/LivrosResources.java
@@ -61,35 +61,17 @@ public class LivrosResources {
 	}
 
 	@RequestMapping(value = "/{id}/comentarios", method = RequestMethod.POST)
-	public ResponseEntity<Void> adicionaComentario(@PathVariable("id") Long livroId, @RequestBody Comentario comentario) {
+	public ResponseEntity<Void> adicionaComentario(@PathVariable("id") Long livroId,
+			@RequestBody Comentario comentario) {
 		livrosService.salvarComentario(livroId, comentario);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
 		return ResponseEntity.created(uri).build();
 
 	}
-	
-	@RequestMapping(value =  "/{id}/comentarios", method = RequestMethod.GET)
-	public ResponseEntity<List<Comentario>> listarComentarios(@PathVariable("id") Long livroId){
+
+	@RequestMapping(value = "/{id}/comentarios", method = RequestMethod.GET)
+	public ResponseEntity<List<Comentario>> listarComentarios(@PathVariable("id") Long livroId) {
 		List<Comentario> comentarios = livrosService.listarComentarios(livroId);
 		return ResponseEntity.status(HttpStatus.OK).body(comentarios);
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
